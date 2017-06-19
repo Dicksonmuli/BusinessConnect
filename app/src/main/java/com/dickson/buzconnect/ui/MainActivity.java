3e77006f5474ce945ec17017bef5b21d8baf3f14
dicksonmuli <dicksonmully6@gmail.com>
2017-06-19 14:51:56 +0300

    adding a carousel on main activity layout

--- app/src/main/java/com/dickson/buzconnect/ui/MainActivity.java
+++ app/src/main/java/com/dickson/buzconnect/ui/MainActivity.java
@@ -3,14 +3,21 @@ package com.dickson.buzconnect.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.dickson.buzconnect.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     //member variables
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    CarouselView carouselView;
+
+    int[] sampleImages = {R.drawable.image_1, R.drawable.image_2, R.drawable.image_3, R.drawable.image_4, R.drawable.image_5};
 
     //binding views
 
@@ -19,10 +26,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 
     @Override
     public void onClick(View v) {
 
     }
+
+
 }
