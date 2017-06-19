6a32d07cd0ec7bf18d781109a1546b893d5e4e1c
dicksonmuli <dicksonmully6@gmail.com>
2017-06-19 11:19:05 +0300

    adding dependencies

--- app/src/main/java/com/dickson/buzconnect/ui/MainActivity.java
+++ app/src/main/java/com/dickson/buzconnect/ui/MainActivity.java
@@ -2,14 +2,27 @@ package com.dickson.buzconnect.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.dickson.buzconnect.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //member variables
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    //binding views
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
