844eaf3d999786d17a632011110437d6fe93f4bb
dicksonmuli <dicksonmully6@gmail.com>
2017-06-19 17:20:57 +0300

    item touch helper view holder defining onItemSelected and onItemClear methods

---
+++ app/src/main/java/com/dickson/buzconnect/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,10 @@
+package com.dickson.buzconnect.util;
+
+/**
+ * Created by dickson on 6/19/17.
+ */
+
+public interface ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
