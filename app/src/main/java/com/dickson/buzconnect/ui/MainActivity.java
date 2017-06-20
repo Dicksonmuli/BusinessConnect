99afca4b0938fe6ad3b4c5efdc8eac87dd43157d
dicksonmuli <dicksonmully6@gmail.com>
2017-06-20 17:09:19 +0300

    inflate overflow menu in the main activity

--- app/src/main/java/com/dickson/buzconnect/ui/MainActivity.java
+++ app/src/main/java/com/dickson/buzconnect/ui/MainActivity.java
@@ -1,10 +1,12 @@
 package com.dickson.buzconnect.ui;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -89,27 +91,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
+    //overriding onClick method
     @Override
     public void onClick(View v) {
+        if(v == mFindListingButton) {
+//                   String location = mLocationEditText.getText().toString();
 
+//                   saveLocationtoFirebase(location);
+//                   if(!(location).equals("")) {
+//                       addToSharedPreferences(location);
+//                   }
+            Intent intent = new Intent(MainActivity.this, ListingListActivity.class);
+//                   intent.putExtra("location", location);
+            startActivity(intent);
+
+        }
+        if (v == mSavedListingButton) {
+            Intent intent = new Intent(MainActivity.this, SavedListingListActivity.class);
+            startActivity(intent);
+        }
     }
-
+    //inflate overflow menu in the main activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
+//            switch (item.getItemId()) {
+//                case R.id.navigation_home:
+//                    mTextMessage.setText(R.string.title_home);
+//                    return true;
+//                case R.id.navigation_dashboard:
+//                    mTextMessage.setText(R.string.title_dashboard);
+//                    return true;
+//                case R.id.navigation_notifications:
+//                    mTextMessage.setText(R.string.title_notifications);
+//                    return true;
+//            }
             return false;
         }
 
