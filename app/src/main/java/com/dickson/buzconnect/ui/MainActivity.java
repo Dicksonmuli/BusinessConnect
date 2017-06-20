71828071aeb72c2ba55dff07d7a1bb28a7fb882d
dicksonmuli <dicksonmully6@gmail.com>
2017-06-20 16:58:06 +0300

    setting onClick listeners in main activity and checking user with firebase auth listener

--- app/src/main/java/com/dickson/buzconnect/ui/MainActivity.java
+++ app/src/main/java/com/dickson/buzconnect/ui/MainActivity.java
@@ -4,16 +4,20 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.dickson.buzconnect.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -27,6 +31,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     int[] sampleImages = {R.drawable.image_1, R.drawable.image_2, R.drawable.image_3, R.drawable.image_4, R.drawable.image_5};
 
     //binding views
+    @Bind(R.id.itemSearchEditText) TextView mItemSearchEditText;
+    @Bind(R.id.locationEditText) TextView mLocationEditText;
+    @Bind(R.id.findListingButton) Button mFindListingButton;
+    @Bind(R.id.navigation_saved) Button mSavedListingButton;
 
 
     @Override
@@ -42,6 +50,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //setting onClick listeners in main activity
+        mFindListingButton.setOnClickListener(this);
+        mSavedListingButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    getSupportActionBar().setTitle("Welcome, " + user.getDisplayName() + "!");
+                }else {
+
+                }
+            }
+        };
 
 
     }
