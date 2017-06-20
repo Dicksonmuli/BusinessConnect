6c96dd7b8ae677fd1345b40422f425ae05d25203
dicksonmuli <dicksonmully6@gmail.com>
2017-06-20 20:12:34 +0300

    ListingDetailFragment - listing object used to set our ImageView and TextViews in onCreateView on onCreate view

--- app/src/main/java/com/dickson/buzconnect/ui/ListingDetailFragment.java
+++ app/src/main/java/com/dickson/buzconnect/ui/ListingDetailFragment.java
@@ -1,20 +1,28 @@
 package com.dickson.buzconnect.ui;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.dickson.buzconnect.Constants;
 import com.dickson.buzconnect.models.Listing;
+import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by dickson on 6/20/17.
@@ -64,6 +72,62 @@ public class ListingDetailFragment extends Fragment implements View.OnClickListe
         mSource = getArguments().getString(Constants.KEY_SOURCE);
         setHasOptionsMenu(true);
     }
+    //    listing object used to set our ImageView and TextViews in onCreateView on onCreate view
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflating the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
+        ButterKnife.bind(this, view);
+
+
+        if (!mRestaurant.getImageUrl().contains("http")) {
+            try {
+                Bitmap image = decodeFromFirebaseBase64(mRestaurant.getImageUrl());
+                mImageLabel.setImageBitmap(image);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // This block of code should already existed, I just moved it to the 'else' statement:
+            Picasso.with(view.getContext())
+                    .load(mRestaurant.getImageUrl())
+                    .resize(MAX_WIDTH, MAX_HEIGHT)
+                    .centerCrop()
+                    .into(mImageLabel);
+        }
+
+        mNameLabel.setText(mRestaurant.getName());
+        mCategoriesLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getCategories()));
+        mRatingLabel.setText(Double.toString(mRestaurant.getRating()) + "/5");
+        mPhoneLabel.setText(mRestaurant.getPhone());
+        mAddressLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getAddress()));
+
+        mWebsiteLabel.setOnClickListener( this);
+        mPhoneLabel.setOnClickListener(this);
+        mAddressLabel.setOnClickListener( this);
+        //setting click listener to saveRestaurant button
+        if (mSource.equals(Constants.SOURCE_SAVED)) {
+            mSaveRestaurantButton.setVisibility(View.GONE);
+        } else {
+            mSaveRestaurantButton.setOnClickListener(this);
+        }
+
+
+        return view;
+    }
+    /**
+     *  take the encoded image's string,
+     *  and use the built-in firebase utility to decode it back into a byte array
+     * @param image
+     * @return
+     * @throws IOException
+     */
+    public static Bitmap decodeFromFirebaseBase64(String image) throws IOException {
+        byte[] decodedByteArray = android.util.Base64.decode(image, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.length);
+    }
+
 
 
 }
