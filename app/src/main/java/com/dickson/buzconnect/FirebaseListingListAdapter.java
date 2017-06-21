a4f71fdfde0e15302d332d1b37161d59bd31e4a4
dicksonmuli <dicksonmully6@gmail.com>
2017-06-21 07:58:36 +0300

    FirebaseListingListAdapter contructor with view holder and database reference

--- app/src/main/java/com/dickson/buzconnect/FirebaseListingListAdapter.java
+++ app/src/main/java/com/dickson/buzconnect/FirebaseListingListAdapter.java
@@ -7,7 +7,10 @@ import com.dickson.buzconnect.util.ItemTouchHelperAdapter;
 import com.dickson.buzconnect.util.OnStartDragListener;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
 
@@ -25,4 +28,41 @@ public class FirebaseListingListAdapter extends FirebaseRecyclerAdapter<Listing,
 
     private ChildEventListener mChildEventListener;
     private ArrayList<Listing> mListings = new ArrayList<>();
+
+    public FirebaseListingListAdapter(Class<Listing> modelClass, int modelLayout,
+                                         Class<FirebaseListingViewHolder> viewHolderClass,
+                                         Query ref, OnStartDragListener onStartDragListener, Context context) {
+        super(modelClass, modelLayout, viewHolderClass, ref);
+        mRef = ref.getRef();//returns the DatabaseReference
+        mOnStartDragListener = onStartDragListener;
+        mContext = context;
+
+//        adding listings with ChildEventListener
+        mChildEventListener = mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                mListings.add(dataSnapshot.getValue(Listing.class));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
